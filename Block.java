51a6d3583c13633c1ad9754683a3ba11dece77f3
Frannx1 <fsanguineti@itba.edu.ar>
2017-09-19 17:38:26 -0300

    docs

--- Block.java
+++ Block.java
@@ -1,23 +1,51 @@
+/**
+ * This class representes the nodes of a @code{Blockchain}
+ * @param <T> The parameter is a generic for the type of the stored data.
+ */
 public class Block <T>{
-    //this class representes the nodes of a blockchain
-    //the T parameter is the type of the stored data.
+
     private long index;// index of the current block
     private T data;
     private long nounce;// the magic number which keeps the hash below a certain number
     private String prevHash;// previous block hash;
     private String hash;
 
-    public Block(int index, T data, String prevHash){
+    /**
+     *
+     * @param index index of this node on the blockchain
+     * @param data infortation to store in this block
+     * @param prevHash the hash in ecode SHA-256 of the previous block
+     */
+    public Block(long index, T data, String prevHash){
         if(index <= 0 || prevHash == null){
             throw new IllegalArgumentException("index or previous hash were incorrect");
         }
-
 
         this.data = data;
         this.index = index;
         this.prevHash = prevHash;
         nounce = 0;
-        hash = null;
+        hash = Encoder.getSha256(Long.toString(index) + data.toString() + prevHash);   //checkear como se hace
+    }
+
+    /**
+     * Finds the nounce that valid the hash of this block
+     */
+    public void mine() {
+        long nounce = 0;
+        hash = Encoder.getSha256(hash + Long.toString(nounce));
+        while(!hash.matches("^0000.*")){
+            nounce++;
+            hash = Encoder.getSha256(hash + Long.toString(nounce));
+        }
+    }
+
+    /**
+     * Allow to know if the hash of this block begins whit 0000*
+     * @return true if the hash of this block begins whit 0000*
+     */
+    public boolean isValidHash() {
+        return hash.matches("^0000.*");
     }
 
     public long getIndex(){
@@ -27,6 +55,7 @@ public class Block <T>{
     public String getHash(){
         return hash;
     }
+
     public T getData(){
         return data;
     }
--- Encoder.java
+++ Encoder.java
@@ -1,34 +1,59 @@
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Encoder {
 
- public static String getSha256(String value) {
-    try{
-        MessageDigest md = MessageDigest.getInstance("SHA-256");
-        md.update(value.getBytes());
-        return bytesToHex(md.digest());
-    } catch(Exception ex){
-        throw new RuntimeException(ex);
+    public static String getSha256(String value) {
+        try{
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(value.getBytes());
+            return bytesToHex(md.digest());
+        } catch(Exception ex){
+            throw new RuntimeException(ex);
+        }
     }
- }
- private static String bytesToHex(byte[] bytes) {
-    StringBuffer result = new StringBuffer();
-    for (byte b : bytes) result.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
-    return result.toString();
- }
 
-
-public static void main(String[] args) {
-    System.out.println("wokring");
-    long magic = 0;
-    String ex = "hello";
-    ex = getSha256(ex);
-    ex = getSha256(ex+magic);
-
-    while(!ex.matches("^0000*")){
-        magic++;
+    private static String bytesToHex(byte[] bytes) {
+        StringBuffer result = new StringBuffer();
+        for (byte b : bytes)
+            result.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
+        return result.toString();
     }
-    System.out.println(ex);
+
+    public static byte[] getSha256(byte[] value) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(value);
+            return md.digest();
+        } catch(NoSuchAlgorithmException ex){
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("wokring");
+        long magic = 0;
+        String ex = "hello";
+        ex = getSha256(ex);
+        ex = getSha256(ex+magic);
+
+        while(!ex.matches("^0000.*")){
+            magic++;
+            ex = getSha256(ex+Long.toString(magic));
+        }
+        System.out.println(ex);
+
+        long magic2 = 0;
+        byte[] ex2 = new byte[32];
+        ex2[3] = 4;
+        ex2 = getSha256(ex2);
+        ex = getSha256(ex+magic);
+
+        while(!ex.matches("^0000.*")){
+            magic++;
+            ex = getSha256(ex2+magic);
+        }
+        System.out.println(ex);
 }
 
 }
